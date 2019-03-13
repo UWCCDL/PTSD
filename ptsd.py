8608f3cceb20877a30320ba4f512a6d7089f41c7
TheRealDrDre <stocco@uw.edu>
2019-03-12 22:25:16 -0700

    Added Briana's code to the python code

--- ptsd.py
+++ ptsd.py
@@ -2,6 +2,32 @@
 
 # A theory of PTSD form a declarative memory standpoint
 
+import numpy as np
+
+def random_memory_generator(num_chunks, num_slots):
+    memories=[]
+    name=[]
+    attributes=['a', 'b', 'c', 'd', 'e', 'f', 'g']
+    a=len(attributes)
+    for i in range(num_chunks):
+        memory=[]
+        slots=[]
+        name=['memory'+str(i)]
+        for j in range(num_slots):
+            random_num=int(round(np.random.uniform(0,a)))
+            random_attribute=attributes[random_num]
+            slots+=['slot'+str(j), str(random_attribute)]
+        memory+=[name+slots]
+        memories+=memory
+
+    return memories
+
+def add_memories(num, num_slots):
+    """Adds NUM memories to the model"""
+    memories = random_memory_generator(num, num_chunks)
+    for m in memories:
+        actr.add_dm(m)
+
 actr.add_dm(["memory3", "f1", "a", "f2", "b"])    
 
 
