c15f660aeb943b314f502a439077391a074ba850
TheRealDrDre <stocco@uw.edu>
2019-03-14 00:28:53 -0700

    Modified code from Briana

--- ptsd.py
+++ ptsd.py
@@ -3,20 +3,21 @@
 # A theory of PTSD form a declarative memory standpoint
 
 import numpy as np
+from random import choice
+import actr
 
 def random_memory_generator(num_chunks, num_slots):
-    memories=[]
-    name=[]
-    attributes=['a', 'b', 'c', 'd', 'e', 'f', 'g']
+    memories = []
+    name = []
+    attributes = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
     a=len(attributes)
     for i in range(num_chunks):
         memory=[]
         slots=[]
-        name=['memory'+str(i)]
+        name=['memory' + str(i + 1)]
         for j in range(num_slots):
-            random_num=int(round(np.random.uniform(0,a)))
-            random_attribute=attributes[random_num]
-            slots+=['slot'+str(j), str(random_attribute)]
+            random_attribute = choice(attributes)
+            slots+=['slot' + str(j + 1), str(random_attribute)]
         memory+=[name+slots]
         memories+=memory
 
@@ -24,10 +25,10 @@ def random_memory_generator(num_chunks, num_slots):
 
 def add_memories(num, num_slots):
     """Adds NUM memories to the model"""
-    memories = random_memory_generator(num, num_chunks)
+    memories = random_memory_generator(num, num_slots)
     for m in memories:
         actr.add_dm(m)
 
-actr.add_dm(["memory3", "f1", "a", "f2", "b"])    
+#actr.add_dm(["memory3", "f1", "a", "f2", "b"])    
 
 
