810a058e133b9deb0bdd7e3a59942a889170674e
Briana <brianam2@uw.edu>
2019-04-16 13:55:33 -0700

    Update ptsd.py

--- ptsd.py
+++ ptsd.py
@@ -2,11 +2,10 @@
 
 # A theory of PTSD form a declarative memory standpoint
 
-import numpy as np
-from random import choice
+import random
 import actr
 
-def random_memory_generator(num_chunks, num_slots):
+def random_memory_generator(mem_num, num_chunks, num_slots):
     memories = []
     name = []
     attributes = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
@@ -14,7 +13,7 @@ def random_memory_generator(num_chunks, num_slots):
     for i in range(num_chunks):
         memory=[]
         slots=[]
-        name=['memory' + str(i + 1)]
+        name=['memory' + str(mem_num + 1) + "_" + str(i + 1)]
         for j in range(num_slots):
             random_attribute = choice(attributes)
             slots+=['slot' + str(j + 1), str(random_attribute)]
@@ -23,12 +22,25 @@ def random_memory_generator(num_chunks, num_slots):
 
     return memories
 
-def add_memories(num, num_slots):
+def add_memories(mem_num, num, num_slots):
     """Adds NUM memories to the model"""
-    memories = random_memory_generator(num, num_slots)
+    memories = random_memory_generator(mem_num, num, num_slots)
     for m in memories:
         actr.add_dm(m)
 
-#actr.add_dm(["memory3", "f1", "a", "f2", "b"])    
+#actr.add_dm(["memory3", "f1", "a", "f2", "b"])
 
+#push random memory set into act-r over the "life time". 1=10 years
+#traumatic event occurs at a random time over the range of life_time
 
+life_time=10
+event_time=randrange(0,life_time,1)
+
+#add memories from that decade to act-r
+
+for t in range(life_time):
+  if t==event_time:
+    #this memory is assigned a high V
+    add_memories(t, 1, 5)
+  else:
+    add_memories(t, 1, 5)
