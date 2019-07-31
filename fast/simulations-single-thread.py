1a73a13bc08dd4ba30f575a893d4aec0997fb540
gardoma <andrea.stocco@gmail.com>
2019-07-31 15:13:59 -0700

    Simulation code running in single thread as well, but does not catch errors

---
+++ fast/simulations-single-thread.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# Meta-code to generate lisp simulations
+
+import os
+
+ACTRDIR="/projects/actr/actr7.x"
+CURRENTDIR=os.path.dirname(os.path.realpath(__file__))
+TEMPLATE="""
+(setf sim (make-instance 'simulation))
+(setf (ptev sim) 
+      '(1 20 10 5 15))
+(setf (ptes sim)
+      '(0 1 0.5 0.25 0.75))
+(let ((ht (make-hash-table)))
+  (setf (gethash :imaginal-activation ht) %.1f)
+  (setf (gethash :bll ht) %.2f)
+  (setf (model-params sim) ht))
+
+(setf (logfile sim) "simulations_w=%.1f_bll=%.2f.txt") 
+(run-simulations sim)
+"""
+
+fout=open("simulations-single-thread.lisp", "w")
+fout.write('(load "%s/load-act-r.lisp")' % ACTRDIR)
+fout.write('(load "%s/ptsd.lisp")' % CURRENTDIR)
+
+for w in [0.0, 10, 5, 2.5, 7.5]:
+    for bll in [0.1, 0.9, 0.5, 0.3, 0.7]:
+        fout.write(TEMPLATE % (w, bll, w, bll))
+        fout.flush()
+fout.write("(quit)")
+fout.flush()
+fout.close()
